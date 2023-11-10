dce44f34b2501f0757b7a06ff7a6af66570e2114
Mamun <md0465981@gmail.com>
2023-11-11 01:04:46 +0600

    added functions for converting the text to upper and lower case and updated the design a bit

--- app/speech-to-text/speechToText.jsx
+++ app/speech-to-text/speechToText.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import styles from "./speechToText.module.css";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import useClipboard from "react-use-clipboard";
 
 const SpeechToText = () => {
@@ -55,21 +54,46 @@ const SpeechToText = () => {
     setIsListening(!isListening);
   };
 
+  // Additional functions
+  const handleDelete = () => {
+    setTranscription("");
+    console.log(transcription);
+  };
+
+  const handleLowerCase = () => {
+    let text = transcription;
+    setTranscription(text.toLowerCase());
+  };
+  const handleUpperCase = () => {
+    let text = transcription;
+    setTranscription(text.toUpperCase());
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.text_box}>
         <h1 className={styles.heading}>Speech to Text</h1>
         <p>Transcription: </p>
         <div className={styles.transcript}>{transcription}</div>
+
+        <button
+          onClick={toggleListening}
+          className={isListening ? styles.stop_btn : styles.start_btn}
+        ></button>
       </div>
 
       <div className={styles.button_box}>
-        <button onClick={toggleListening} className={styles.btn}>
-          {isListening ? "Stop Listening" : "Start Listening"}
-        </button>
-
         <button className={styles.btn} onClick={setCopied}>
           Copy
+        </button>
+        <button className={styles.btn} onClick={handleLowerCase}>
+          Lower case
+        </button>
+        <button className={styles.btn} onClick={handleUpperCase}>
+          Upper case
+        </button>
+        <button className={styles.btn} onClick={handleDelete}>
+          Delete
         </button>
       </div>
     </div>
