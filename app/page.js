801d224cc03b79a1d0842ba5fc3535c47240fcf7
Mamun <md0465981@gmail.com>
2024-01-13 05:06:31 +0600

    deteled all testing files and returned to the last update

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import React from "react";
 import SpeechToText from "./speech-to-text/speechToText";
-import ReservationForm from "./reservationForm/reservationForm";
 
 function Home() {
   return (
     <>
-      {/* <SpeechToText></SpeechToText> */}
-      <ReservationForm />
+      <SpeechToText></SpeechToText>
     </>
   );
 }
